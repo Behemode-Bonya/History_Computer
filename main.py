ae4495054268ddca7517511a1148e9bbdfc874ee
Behemode-Bonya <karinakaa1966@gmail.com>
2025-12-03 18:53:12 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+import pdfplumber
+from gtts import gTTS
+
+
+def get_text():
+    with pdfplumber.open("andrew.pdf") as file:
+        pages = [page.extract_text() for page in file.pages]
+        text = " ".join(pages).replace("\n"," ")
+    return text
+
+
+def get_audio_file(text):
+    language = "ru"
+    phrase = gTTS(text = text,lang = language,slow = False)
+    phrase.save("Welcome.mp3")
+
+
+def main():
+    text = get_text()
+    print(text)
+    get_audio_file(text)
+
+
+if __name__ == "__main__":
+    main()
